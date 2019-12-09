c5ded92167849d08565a1846020054043cb5a26b
amarv <amarvadla27@gmail.com>
2019-12-09 22:06:35 +0530

    example 4

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,20 +5,35 @@ import { CourseService } from './courses.service';
     selector: 'courses',
     template: `
     <h2>{{ getTitle() }}</h2>
-    <ul>
-        <li *ngFor="let course of courses"> 
-        {{ course }}
-        </li>
-    </ul>
+    <input (keyup) = "onKeyUp($event)" />
     `
 })
+
+// <button class="btn btn-primary" [class.active] = "isActive()" [style.backgroundColor] = " isActive() ? 'red' : 'blue' ">Save</button>
+// <img [src] = "imageUrl"/>
+// <button (click) = "onSave($event)">Save</button>
 
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    imageUrl = "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
 
     getTitle(){
         return this.title;
+    }
+
+    isActive(){
+        return true;
+    }
+
+    onSave($event){
+        console.log($event);
+    }
+
+    onKeyUp($event){
+        if($event.keyCode === 13){
+            console.log("Enter was pressed")
+        }
     }
 
     constructor(service: CourseService){

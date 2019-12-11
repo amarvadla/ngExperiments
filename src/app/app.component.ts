209c1bbf15cab07dc3f138141916352d6afff64d
amarv <amarvadla27@gmail.com>
2019-12-11 08:19:27 +0530

    66 - changes in ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,13 @@ export class AppComponent {
     isFavorite: true
   }
 
+  courses = [{ id: 1, name: 'angular' }, { id: 2, name: 'node' }]
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index, 1)
+    console.log(index)
+  }
   onFavoriteChanged(isFavorite: boolean) {
     console.log(isFavorite);
   }

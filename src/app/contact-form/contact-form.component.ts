30ae740a671d484913ccba21ad25c7b54c81da11
amarnath <avadla@gemini-us.com>
2019-12-12 15:37:45 +0530

    finished with contact-form component

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent {
 
-  log(x){
+  list = [
+    { id: 1, name: "Email" },
+    { id: 2, name: "Phone" },
+    { id: 3, name: "LandLine" }
+  ]
+
+  log(x) {
     console.log(x)
+  }
+
+  submit(form) {
+    if (form.valid) {
+      console.log(form.form.value);
+    }
   }
 
 }
